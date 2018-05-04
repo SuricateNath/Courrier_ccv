a309fb03b5a044fd2ec1aecc0d61d558296885b7
Nathalie <natybiz-80@hotmail.fr>
2018-05-04 10:27:24 +0200

    Fonction-search-données
    Ajout d'un champs de recherche de données ds la collection. Seuls les enregistrements comprenant le texte recherché sont affichés

---
+++ imports/ui/body.js
@@ -0,0 +1,236 @@
+import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+
+import { Lettres } from '../api/lettres.js';
+
+import './body.html';
+
+// ici on declare des variables qui appellent 
+//la methode reactive dict et renvoi le résultat
+
+var pageSession = new ReactiveDict();
+var lettres = Lettres.find({});
+
+// appel de la fonction search
+var LettresViewItems = function(cursor){
+    if(!cursor){
+      return [];
+    }
+
+    // mémo:pageSession est la var qui appelle le reactivedict
+    var searchString = pageSession.get("LettresViewSearchString");
+
+    var raw = cursor.fetch();
+
+    var filtered = [];
+
+    if(!searchString || searchString == ""){
+      filtered = raw; // idem: filtered = cursor.fetch();
+    } else {
+
+      // info: transfo des potentiels searchString en ignorant les . pour le codes
+      searchString = searchString.replace(".", "\\.");
+
+      var regEx = new RegExp(searchString, "i"); // sert à ne pas prendre en compte la casse du mot recherché
+      var searchFields = ["text", "objet", "exp"];
+
+      filtered = _.filter(raw, function(item){
+
+          var match = false;
+
+          _.each(searchFields, function(field){
+
+              var value = (getPropertyValue(field, item) || "") + "";
+
+              match = match || (value && value.match(regEx));
+              
+              if(match){
+              return false;
+              }
+          })
+        return match;
+
+      });
+      // fin du else
+    }
+     
+      return filtered;
+};
+
+Template.body.helpers({
+   LettresFiltered(){
+    return LettresViewItems(lettres);
+    },
+  count() {
+      return Lettres.find({}).count(); // ok nombre de courriers
+    },
+    // compteur des courriers a traiter
+  incompleteCount() {
+      return Lettres.find( { traite: { $eq: ''  } }).count( ); //ok - nbre sans réponse envoyée
+  },
+    // compteur des courriers avec réponse ce jour. - ok
+  todayCount() {    
+    return Lettres.find({ deadline: { $eq:today } }).count();
+  },
+  tomorrowCount() {    
+    return Lettres.find({ deadline: { $gt:today } }).count();
+  },
+  // compteur des réponses hors délai.
+  deadlineCount() { 
+    return Lettres.find({ deadline: {$lt: today } }).count();
+  //$or: [{end_date: null}, {end_date: {$gte: 1376982000}}]
+  }
+});
+
+
+
+
+var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //Janvier = 0 
+    var yyyy = today.getFullYear();
+      if(dd<10) {
+        dd = '0'+dd
+      } 
+      if(mm<10) {
+        mm = '0'+mm
+      } 
+    today = dd + '/' +mm + '/' + yyyy;
+
+Template.body.events({
+	'submit .new-task'(event){
+		event.preventDefault();
+
+   let date =  moment(event.target.date.value).format('DD/MM/YYYY');
+    if(!date){ alert("Veuillez saisir la date d'enregistrement svp !"); stop.propagation();}  
+    console.log('date : ' + date);    
+
+    let exp = event.target.exp.value;
+    if(!exp){ alert("Veuillez saisir l'expéditeur svp !"); stop.propagation();}
+
+    let dest = event.target.dest.value;
+    if(!dest){ alert("Veuillez saisir le destinataire svp !"); stop.propagation();}
+
+    let objet = event.target.objet.value;
+    if(!objet){ alert("Veuillez saisir l'objet du courrier svp !"); stop.propagation();}
+
+    let deadline = moment(event.target.deadline.value).format('DD/MM/YYYY');
+    let obs = event.target.obs.value;
+    let traite = event.target.traite.value;
+    
+   console.log(dest);
+   console.log(exp);
+   console.log(objet);
+	
+    if(dest !=''){
+  		  Lettres.insert({
+  			  date:date,
+          exp: exp,
+          dest: dest,
+          objet: objet,
+          deadline: deadline,
+          obs: obs,   
+          traite: traite,       
+  			});
+
+		  event.target.date.value = '';
+      event.target.exp.value = '';
+      event.target.dest.value = '';
+      event.target.objet.value = '';
+      event.target.deadline.value = '';   
+      event.target.obs.value = '';
+      event.target.traite.value = '';
+       }
+	},
+
+  'keydown .search': function(e, t){
+
+    // === 13 : Enter
+    if(e.which === 13)
+    {
+      e.preventDefault();
+      var form = $(e.currentTarget).parent();
+      if(form) {
+        var searchInput = form.find("#search-input");
+        if(searchInput) {
+          var searchString = searchInput.val();
+          pageSession.set("LettresViewSearchString", searchString);
+        }
+
+      }
+      return false;
+    }
+
+
+    // === 27 : "Escape"
+
+    if(e.wich === 27){
+      event.preventDefault();
+
+      var form = $(e.currentTarget).parent();
+
+        if(form){
+          var searchInput = form.find("#search-input");
+
+            if(searchInput){
+            searchInput.val("");
+            pageSession.set("LettresViewSearchString", "");
+        }
+
+    }
+
+    return false;
+
+    }
+
+  return true; // donc envoi du submit
+  }
+
+});
+
+
+
+ 
+Template.lettre.events({
+  'click .list-courrier'(event, instance){
+    console.log(instance.data);
+    // ici modal permet d'ouvrir une fenetre de modification de champs
+    Modal.show('modal_show_courrier', instance.data); // instance.data sert à accéder aux valeurs du texte
+  }
+});
+
+
+
+Template.modal_show_courrier.events({
+  'submit .js-edit-courrier' (event, instance){
+    event.preventDefault();
+
+    var Id_courrier = instance.data._id; // ici on créé une variable qui permet de recupérer l'id de la ligne modifiée.
+
+    // la confirmation de la sauvergarde est appelée et update la bdd
+    Lettres.update({ 
+      _id: Id_courrier}, {
+      $set:{
+        date: event.target.date.value,
+        exp: event.target.exp.value,
+        dest: event.target.dest.value,
+        objet: event.target.objet.value,
+        deadline: event.target.deadline.value,
+        obs: event.target.obs.value,
+        traite: event.target.traite.value
+      }
+    });
+
+    Modal.hide();
+  },
+
+  'click .js-delete-courrier' (event, instance){
+
+    var reponse = window.confirm("Attention cette suppression sera définitive, confirmez vous la suppression ?")
+
+    if(reponse == true){
+      Lettres.remove({_id: instance.data._id});}
+  
+    Modal.hide();
+  }
+});
--- server/main.js
+++ server/main.js
@@ -1,6 +1,1 @@
-import { Meteor } from 'meteor/meteor';
-//import '../imports/api/courriers.js';
-
-Meteor.startup(() => {
-  // permet l'autorun de l'appli 
-});
+import '../imports/api/lettres.js';
